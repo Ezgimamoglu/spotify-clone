3338ed7c490bc48dd753359a336f7929719504a7
ezgimamoglu <ezgi.imamoglu@bilgiedu.net>
2020-10-20 00:32:33 +0300

    spotify-clone commit

---
+++ src/reducer.js
@@ -0,0 +1,34 @@
+export const initialState = {
+  user: null,
+  playlist: [],
+  playing: false,
+  item: null,
+  //token:
+  //   "BQAPE-koQPedyxiUouhqfvdTz2HsEei6EeYLOAY_qk01T-Mj37…L2l9EygsiUkTzW5Bv3y1zsoJVqwSXtXjAuvN2Sz4qm0is9x8Y",
+};
+
+const reducer = (state, action) => {
+  console.log(action);
+
+  switch (action.type) {
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.user,
+      };
+    case "SET_TOKEN":
+      return {
+        ...state,
+        token: action.token,
+      };
+    case "SET_PLAYLISTS":
+      return {
+        ...state,
+        playlists: action.playlists,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
